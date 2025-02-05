cccfbdcea16d7a11ad81c9dff9463750c5f6c402
paulaallemand <paulaisdev@gmail.com>
2025-02-05 19:42:13 -0300

    Adiciona validador de parcelas

--- src/main/java/com/insurance/api/utils/validator/ParcelaValidator.java
+++ src/main/java/com/insurance/api/utils/validator/ParcelaValidator.java
@@ -1,4 +1,15 @@
 package com.insurance.api.utils.validator;
 
+import com.insurance.api.model.Parcela;
+import jakarta.validation.ValidationException;
+
 public class ParcelaValidator {
+    public static void validarParcela(Parcela parcela) {
+        if (parcela.getApolice() == null) {
+            throw new ValidationException("Parcela não vinculada a uma apólice.");
+        }
+        if (!"PENDENTE".equalsIgnoreCase(parcela.getSituacao())) {
+            throw new ValidationException("Parcela já foi paga ou está inválida.");
+        }
+    }
 }
