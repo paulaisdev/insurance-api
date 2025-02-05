5bbc6cf93709f3ab169b62861faaa1dc53944722
paulaallemand <paulaisdev@gmail.com>
2025-02-05 19:41:13 -0300

    Adiciona parser de parcelas em CSV

--- src/main/java/com/insurance/api/service/csv/ParcelaCsvParser.java
+++ src/main/java/com/insurance/api/service/csv/ParcelaCsvParser.java
@@ -1,4 +1,31 @@
 package com.insurance.api.service.csv;
 
-public class ParcelaCsvParser {
+import com.insurance.api.model.Parcela;
+import com.insurance.api.utils.CsvParser;
+import com.insurance.api.utils.validator.ApoliceValidator;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParcelaCsvParser implements CsvParser<Parcela> {
+
+    @Override
+    public List<Parcela> parse(String[] dados) {
+        List<Parcela> parcelas = new ArrayList<>();
+
+        for (int i = 5; i < dados.length; i += 3) {
+            if (i + 2 >= dados.length) {
+                break;
+            }
+
+            Parcela parcela = new Parcela();
+            parcela.setPremio(ApoliceValidator.converterPremio(dados[i]));
+            parcela.setDataPagamento(LocalDate.parse(dados[i + 1].trim()));
+            parcela.setFormaPagamento(dados[i + 2].trim());
+
+            parcelas.add(parcela);
+        }
+        return parcelas;
+    }
 }
